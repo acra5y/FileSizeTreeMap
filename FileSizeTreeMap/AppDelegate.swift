94736b20db62da0bd602ad782e5c3aa245a942f6
acra5y <oli4@acrasy.net>
2019-09-01 18:31:03 +0200

    Init project

---
+++ FileSizeTreeMap/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FileSizeTreeMap
+//
+//  Created by Olivier Schmelzle on 01.09.19.
+//  Copyright © 2019 Olivier Schmelzle. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ FileSizeTreeMap/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FileSizeTreeMap
+//
+//  Created by Olivier Schmelzle on 01.09.19.
+//  Copyright © 2019 Olivier Schmelzle. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
